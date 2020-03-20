6f100e92f3b28b7d3ba49f96b696eb5f460ad646
GutierrezPalizaJosePablo <37850614+pablogupa@users.noreply.github.com>
2020-03-20 12:48:59 -0600

    practica agregada

---
+++ Firebase/basededatos.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJzExgYR3kegJWfxYmWx-iB9ZHERZTVg0",
+    authDomain: "proyectomaha-65732.firebaseapp.com",
+    databaseURL: "https://proyectomaha-65732.firebaseio.com",
+    projectId: "proyectomaha-65732",
+    storageBucket: "proyectomaha-65732.appspot.com",
+    messagingSenderId: "241976430088",
+    appId: "1:241976430088:web:cbae3b5cb7453e7fbbcbbd"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
